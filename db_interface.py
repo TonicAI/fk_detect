d9d427b6c18045758cefbec3761519a062c205c0
Andrew Colombi <acolombi@gmail.com>
2019-06-05 10:59:10 -0700

    bug fixes

--- db_interface.py
+++ db_interface.py
@@ -132,17 +132,21 @@ class PostgresDbInterface:
                                         tar.oid AS target_table_id,
                                         tar.relnamespace AS target_schema_id,
                                         tar.relname AS target_table,
-                                        unnest(con.confkey) as target_column_id
+                                        unnest(con.confkey) as target_column_id,
+
+                                        con.connamespace AS constraint_nsp,
+                                        con.conname AS constraint_name
 
                                     FROM pg_constraint con
                                     JOIN pg_class fk ON con.conrelid = fk.oid
                                     JOIN pg_class tar ON con.confrelid = tar.oid
+                                    WHERE con.contype = 'f'
                                 ) sub
                                 JOIN pg_attribute fk_att ON fk_att.attrelid = fk_table_id AND fk_att.attnum = fk_column_id
                                 JOIN pg_attribute tar_att ON tar_att.attrelid = target_table_id AND tar_att.attnum = target_column_id
                                 JOIN pg_namespace fk_nsp ON fk_schema_id = fk_nsp.oid
                                 JOIN pg_namespace tar_nsp ON target_schema_id = tar_nsp.oid
-                                GROUP BY 1, 2, 4, 5;""")
+                                GROUP BY 1, 2, 4, 5, sub.constraint_nsp, sub.constraint_name;""")
 
             return tupleize(cursor.fetchall())
 
